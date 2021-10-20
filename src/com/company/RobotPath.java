da7cc1081841f75e05491bed217478a147a6aaef
William <wament@iastate.edu>
2021-10-20 14:04:26 -0500

    worked on some of the input handling

--- src/com/company/RobotPath.java
+++ src/com/company/RobotPath.java
@@ -8,6 +8,13 @@ public class RobotPath {
 
         File f;
         Scanner scan;
+        String [][] grid;
+        int nrows;
+        int ncols;
+        int [] start = new int[2];
+        int [] dest = new int[2];
+
+
         public RobotPath(){
 
         }
@@ -22,8 +29,37 @@ public class RobotPath {
                 System.out.println("file not found");
             } finally {
                 System.out.println("File read successfully.");
-                System.out.println(scan.nextLine());
+            }
+
+            scan.nextLine();
+            nrows = scan.nextInt();
+            ncols = scan.nextInt();
+            grid = new String[nrows][ncols];
+            scan.nextLine();
+            start[0] = scan.nextInt();
+            start[1] = scan.nextInt();
+            grid[start[0]][start[1]] = "S";
+            scan.nextLine();
+            dest[0] = scan.nextInt();
+            dest[1] = scan.nextInt();
+            grid[dest[0]][dest[1]] = "D";
+
+            scan.nextLine();
+            while(scan.nextLine() != null){
 
             }
+
+        }
+
+        public void planShortest(){
+
+        }
+
+        public void quickPlan(){
+
+        }
+
+        public void output(){
+
         }
 }
