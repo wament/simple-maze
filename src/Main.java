e599d8229132b525114c91f5dad2ac76bf74d965
William <wament@iastate.edu>
2021-10-30 21:12:30 -0500

    removed the unneccessary package

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-package com.company;
-
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         RobotPath rob = new RobotPath();
-        //rob.readInput("C:\\Users\\William\\Documents\\Algo311\\311hw4\\src\\com\\company\\input12.txt");
-        //rob.readInput("C:\\Users\\will\\Documents\\Algo311\\hw4\\src\\com\\company\\input.txt");
+        //rob.readInput("C:\\Users\\William\\Documents\\Algo311\\311hw4\\src\\input2.txt");
+        //rob.readInput("C:\\Users\\will\\Documents\\Algo311\\hw4\\src\\input.txt");
         rob.readInput(args[0]);
+
         rob.quickPlan();
         rob.output();
 
